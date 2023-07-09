59ef3963dbc1b8370ad736d93fb894541004c487
theaashishh <ashish12829@gmai.com>
2023-07-10 00:12:28 +0530

    i think its done maybe not.

--- lnktree/src/components/Lnktree.jsx
+++ lnktree/src/components/Lnktree.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+React;
 import bgimg from '../assets/bgimg.webp'
 import Pfpicture from '../assets/9.png'
-
+import instaImg from '../assets/icon/insta.png'
+import ytImg from '../assets/icon/youtube.avif'
+import webImg from '../assets/icon/website.webp'
+import newsImg from '../assets/icon/news.webp'
 
 
 function Lnktree() {
@@ -21,37 +25,37 @@ function Lnktree() {
                 {/* link container */}
 
                 <div className="main_lnk_container">
-                    <div className="insta-lnk lnk-box">
+                    <a className="insta-lnk lnk-box" href='https://www.instagram.com/puneetsuper_starrrr/?hl=en' target='_blank' rel="noopener noreferrer">
                         <div className="icon-box">
-                            <img src={bgimg}  />
+                            <img src={instaImg} />
                         </div>
                         <h1 className="title">Instagram</h1>
                         <span></span>
-                    </div>
+                    </a>
 
-                    <div className="yt-lnk lnk-box">
+                    <a className="yt-lnk lnk-box " href='https://www.youtube.com/@puneetsuperstar6150' target='_blank' rel="noopener noreferrer">
                         <div className="icon-box">
-                            <img src={bgimg}  />
+                            <img src={ytImg} />
                         </div>
                         <h1 className="title">Youtube</h1>
                         <span></span>
-                    </div>
+                    </a>
 
-                    <div className="website-lnk lnk-box">
+                    <a className="website-lnk lnk-box" href='https://lordpuneet.in/' target='_blank' rel="noopener noreferrer">
                         <div className="icon-box">
-                            <img src={bgimg}  />
+                            <img src={webImg} />
                         </div>
                         <h1 className="title">Lordpuneet Site</h1>
                         <span></span>
-                    </div>
+                    </a>
 
-                    <div className="bb-news-lnk lnk-box">
+                    <a className="bb-news-lnk lnk-box" href='https://news.abplive.com/entertainment/ott/bigg-boss-ott-2-puneet-superstar-evicted-in-less-than-12-hours-1609865' target='_blank' rel="noopener noreferrer">
                         <div className="icon-box">
-                            <img src={bgimg}  />
+                            <img src={newsImg} />
                         </div>
                         <h1 className="title">BB OTT News</h1>
                         <span></span>
-                    </div>
+                    </a>
 
                 </div>
             </div>
